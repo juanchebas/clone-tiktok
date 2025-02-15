3e684d3b47ce57d36f2e43a108fe202c699b3b39
Juan Sebastian Torres Rozo <130316313+juanchebas@users.noreply.github.com>
2025-02-15 09:44:16 -0500

    Add files via upload

---
+++ Proyecto tiktok/proyecto/app.js
@@ -0,0 +1,12 @@
+//envia recursos multimedia a la seccion de main si se desea
+
+// let main = document.querySelector("main");
+
+// let i = 0;
+
+// while (i < 15) {
+//     let img = document.createElement("img");
+//     img.src="./img/img-tik-tok.jpeg"
+//     main.appendChild(img)
+//     i++
+// }
